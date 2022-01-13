1ab7a3b8d6230958958119508f92025faca6fec9
KDavlatjon <davlatjonochilov74@gmail.com>
2022-01-13 12:56:41 +0500

    added config

---
+++ src/main/java/restfull/config/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package restfull.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import restfull.security.JwtConfigurer;
+import restfull.security.JwtTokenProvider;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfiguration(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/logIn").permitAll()
+                .antMatchers("/api/register").permitAll()
+                .antMatchers("/customerWeb/signIn").permitAll()
+                .antMatchers("/api//customerWeb/signUp").permitAll()
+                .antMatchers("/api/students/all").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
+    }
+
+}
